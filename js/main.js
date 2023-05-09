9079251384dab2a403d89431220a3e897f9cc4ce
Deepak Meena <deepakdude582@gmail.com>
2023-05-09 13:05:37 +0530

    Added coin support.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,176 @@
 window.addEventListener("load", function () {
   // Get the device pixel ratio
-  const debug = false;
   const dpr = window.devicePixelRatio || 1;
   const canvas = document.getElementById("canvas0");
   const mainMenu = document.getElementById("mainMenu");
+  // Buttons
+  const mcButton = document.getElementById("mc-btn");
+  const skaterButton = document.getElementById("skater-btn");
+  const wizardButton = document.getElementById("wizard-btn");
   const againMc = document.getElementById("again-mc-btn");
   const againSkater = document.getElementById("again-skater-btn");
   const againWizard = document.getElementById("again-wizard-btn");
+  // Audios
   const startAudio = document.getElementById("start-audio");
   const middleAudio = document.getElementById("middle-audio");
   const endAudio = document.getElementById("end-audio");
-
+  // Screens
   const gameOverMenu = document.getElementById("gameOverMenu");
   const scoreCount = document.getElementById("score");
+  const coinElement = document.getElementById("coinElement");
+  // Canvas setup
   const ctx = canvas.getContext("2d");
   canvas.width = canvas.clientWidth * dpr;
   canvas.height = canvas.clientHeight * dpr;
-  let gameSpeed = 14;
+  // Init variables
   let highScore = localStorage.getItem("highScore") || 0;
+  let gameSpeed = 14;
   let avatar = "mc";
   let gameOver = false;
   let enemies = [];
+  let coins = [];
   let score = 0;
+  let input;
+  let player;
+  let background;
+  let lastTime;
+  let enemyTimer;
+  let enemyInterval;
+  let coinTimer;
+  let coinInterval;
+  let randomCoinInterval;
+  let randomEnemyInterval;
+  let animationId;
+  let lastFrameTime;
+  let scoreCounter;
+  let coinCount;
 
+  // Debug
+  const debug = true;
+  const debugGameOver = false;
+  const debugRun = false;
+  const debugSpeed = null;
+
+  // Utils
+  function randomNumberGen(min, max) {
+    // min and max included
+    return Math.random() * (max - min) + min;
+  }
+
+  function getRandomIntInRange(min, max) {
+    // Ensure min and max are integers
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    // Generate random integer within the range (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function getObstacleInterval(gameSpeed) {
+    const minInterval = 1000;
+    let obstacleInterval = (1 / gameSpeed) * 25000;
+    obstacleInterval = Math.max(obstacleInterval, minInterval);
+    if (obstacleInterval <= 1000) {
+      obstacleInterval = getRandomIntInRange(500, obstacleInterval);
+    }
+    return obstacleInterval;
+  }
+
+  function circleRectCollision(
+    circleX,
+    circleY,
+    circleRadius,
+    rectX,
+    rectY,
+    rectWidth,
+    rectHeight
+  ) {
+    // Calculate the distance between the center of the circle and the closest point on the rectangle
+    let distX = Math.abs(circleX - rectX - rectWidth / 2);
+    let distY = Math.abs(circleY - rectY - rectHeight / 2);
+
+    // If the distance is greater than the sum of the radius and half the rectangle's width or height, there is no collision
+    if (
+      distX > rectWidth / 2 + circleRadius ||
+      distY > rectHeight / 2 + circleRadius
+    ) {
+      return false;
+    }
+
+    // If the distance is less than or equal to the radius of the circle, check if the closest point on the rectangle is inside the circle
+    if (distX <= rectWidth / 2 || distY <= rectHeight / 2) {
+      return true;
+    }
+
+    // Check if the closest point on the rectangle is inside the circle
+    let dx = distX - rectWidth / 2;
+    let dy = distY - rectHeight / 2;
+    return dx * dx + dy * dy <= circleRadius * circleRadius;
+  }
+
+  function fixEnemyCoinOverlap(enemies, coins) {
+    enemies.forEach((enemy) => {
+      coins.forEach((coin) => {
+        if (
+          coin.collisionBox.x <
+            enemy.collisionBox.x + enemy.collisionBox.width &&
+          coin.collisionBox.x + coin.collisionBox.width > enemy.collisionBox.x
+        ) {
+          coin.x = getRandomIntInRange(coin.x + 250, coin.x + 500);
+        }
+      });
+    });
+  }
+
+  function initGame(name, restart = false) {
+    // Shared Code
+    endAudio.pause();
+    endAudio.currentTime = 0;
+    if (!debug) {
+      startAudio.play();
+    }
+    avatar = name;
+    input = new InputHandler();
+    player = new Player(canvas.width, canvas.height);
+    background = new Background(canvas.width, canvas.height);
+
+    score = 0;
+    coinCount = 0;
+
+    lastTime = 0;
+    enemyTimer = 0;
+    enemyInterval = 100;
+    randomEnemyInterval = Math.random() * 1000 + 500;
+    lastFrameTime = 0;
+    scoreCounter = 0;
+    coinTimer = 0;
+    coinInterval = 100;
+    randomCoinInterval = Math.random() * 1000 + 500;
+
+    gameSpeed = debugSpeed || 14;
+    enemies = [];
+    coins = [];
+
+    gameOver = false;
+    if (restart) {
+      // Restart code
+      gameOverMenu.classList.remove("show");
+    } else {
+      // Firs start code
+      mainMenu.classList.remove("show");
+    }
+
+    animate(0);
+  }
+
+  const againFunction = (name) => {
+    initGame(name, true);
+  };
+
+  function setAvatar(name) {
+    initGame(name, false);
+  }
+
+  // Classes
   class InputHandler {
     constructor() {
       this.keys = [];
@@ -114,8 +262,9 @@ window.addEventListener("load", function () {
       );
     }
 
-    update(input, deltaTime, enemies) {
+    update(input, deltaTime, enemies, coins) {
       // Collision detection
+      // Enemies
       enemies.forEach((enemy) => {
         if (
           this.collisionBox.x <
@@ -126,7 +275,9 @@ window.addEventListener("load", function () {
             enemy.collisionBox.y + enemy.collisionBox.height &&
           this.collisionBox.height + this.collisionBox.y > enemy.collisionBox.y
         ) {
-          gameOver = true;
+          if (!debugGameOver) {
+            gameOver = true;
+          }
         }
 
         if (
@@ -142,6 +293,28 @@ window.addEventListener("load", function () {
         ) {
           // this.frameY = 2;
         }
+      });
+      // Coins
+      const collisionIndexes = [];
+      coins.forEach((coin, index) => {
+        const collide = circleRectCollision(
+          coin.collisionBox.x,
+          coin.collisionBox.y,
+          coin.collisionBox.radius,
+          this.collisionBox.x,
+          this.collisionBox.y,
+          this.collisionBox.width,
+          this.collisionBox.height
+        );
+        if (collide) {
+          collisionIndexes.push(index);
+          coinCount++;
+          coinElement.innerHTML = coinCount;
+        }
+      });
+
+      collisionIndexes.forEach((collision) => {
+        coins.splice(collision, 1);
       });
 
       // Sprite animation
@@ -352,10 +525,100 @@ window.addEventListener("load", function () {
         width: this.width - 130 / this.size,
         height: this.height - 100 / this.size,
       };
+
+      this.speed = gameSpeed;
     }
   }
 
-  function handleEnemies(deltaTime) {
+  class Coin {
+    constructor(gameWidth, gameHeight, size = 1, number = 0) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.size = size;
+      this.width = 200;
+      this.height = 200;
+      this.image = document.getElementById("coin");
+      this.x = this.gameWidth + number;
+      this.offsetY = -10;
+      this.y = this.gameHeight - this.height * this.size + this.offsetY;
+      this.speed = gameSpeed;
+      this.frameX = 0;
+      this.maxFrame = 0;
+      this.fps = 20;
+      this.frameTimer = 0;
+      this.frameInterval = 1000 / this.fps;
+      this.markedForDeletion = false;
+      this.collisionBox = {
+        x: this.x,
+        y: this.y,
+        width: this.width * this.size,
+        height: this.height * this.size,
+        radius: (this.width * this.size) / 2,
+      };
+    }
+
+    draw(context) {
+      if (debug) {
+        context.strokeStyle = "red";
+        context.strokeRect(
+          this.collisionBox.x,
+          this.collisionBox.y,
+          this.collisionBox.width,
+          this.collisionBox.height
+        );
+      }
+      context.beginPath();
+      context.arc(
+        this.x + this.collisionBox.width / 2,
+        this.y + this.collisionBox.height / 2,
+        this.collisionBox.width / 2,
+        0,
+        Math.PI * 2
+      );
+      context.stroke();
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        0,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width * this.size,
+        this.height * this.size
+      );
+    }
+
+    update(deltaTime) {
+      if (this.frameTimer > this.frameInterval) {
+        if (this.frameX >= this.maxFrame) this.frameX = 0;
+        else this.frameX++;
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += deltaTime;
+      }
+      this.x -= this.speed;
+      if (this.x < 0 - this.width) {
+        this.markedForDeletion = true;
+
+        // Increase speed
+        gameSpeed += Math.pow(0.9, gameSpeed);
+        if (gameSpeed > 25) gameSpeed = 25;
+      }
+      this.collisionBox = {
+        x: this.x,
+        y: this.y,
+        width: this.width * this.size,
+        height: this.height * this.size,
+        radius: (this.width * this.size) / 2,
+      };
+      this.speed = gameSpeed;
+    }
+  }
+
+  // Game functions
+  function handleObjects(deltaTime) {
+    // enemy
     if (enemyTimer > enemyInterval + randomEnemyInterval) {
       let random = Math.ceil(randomNumberGen(0, 2));
       let size = randomNumberGen(1.2, 0.8);
@@ -367,28 +630,13 @@ window.addEventListener("load", function () {
           distance = (enemies.at(-1).width * enemies.at(-1).size) / 2;
           if (i > 0) {
             continue;
-            distance = enemies.at(-1).width * enemies.at(-1).size;
-            console.log(distance);
           }
           enemies.push(new Enemy(canvas.width, canvas.height, size, distance));
         }
       }
 
-      // size = randomNumberGen(1.2, 0.8);
-      // distance = (enemies.at(-1).width * enemies.at(-1).size) / 2;
-      // enemies.push(new Enemy(canvas.width, canvas.height, size, distance));
-
       enemyTimer = 0;
-      randomEnemyInterval =
-        Math.random() *
-          (1000 - gameSpeed * 10 < 10 ? 10 : 1000 - gameSpeed * 10) +
-        (2500 - gameSpeed * 100 < 10 ? 10 : 2500 - gameSpeed * 100);
-      // console.log({
-      //   randomEnemyInterval,
-      //   gameSpeed,
-      //   rand: 1000 - gameSpeed * 10,
-      //   offset: 2500 - gameSpeed * 100 < 10 ? 10 : 2500 - gameSpeed * 100,
-      // });
+      randomEnemyInterval = getObstacleInterval(gameSpeed);
     } else {
       enemyTimer += deltaTime;
     }
@@ -405,6 +653,26 @@ window.addEventListener("load", function () {
     });
 
     enemies = enemies.filter((enemy) => !enemy.markedForDeletion);
+
+    // Coins
+    if (coinTimer > coinInterval + randomCoinInterval) {
+      coins.push(new Coin(canvas.width, canvas.height, 0.25, 0));
+
+      coinTimer = 0;
+      randomCoinInterval = getObstacleInterval(
+        gameSpeed * randomNumberGen(1, 2)
+      );
+    } else {
+      coinTimer += deltaTime;
+    }
+
+    fixEnemyCoinOverlap(enemies, coins);
+    coins.forEach((coin) => {
+      coin.draw(ctx);
+      coin.update(deltaTime);
+    });
+
+    coins = coins.filter((coin) => !coin.markedForDeletion);
   }
 
   function displayStatusText(context) {
@@ -417,20 +685,15 @@ window.addEventListener("load", function () {
     context.fillText("Score: " + score, 20, 90);
     context.fillStyle = "white";
     context.fillText("Score: " + score, 22, 92);
+
+    // Coins
+    context.fillStyle = "black";
+    context.fillText("Coins: " + coinCount, 20, 130);
+    context.fillStyle = "white";
+    context.fillText("Coins: " + coinCount, 22, 132);
   }
 
-  let input;
-  let player;
-  let background;
-
-  let lastTime;
-  let enemyTimer;
-  let enemyInterval;
-  let randomEnemyInterval;
-  let animationId;
-  let lastFrameTime;
-  let scoreCounter;
-
+  // Main loop
   function animate(timeStamp) {
     if (gameOver) {
       if (score > highScore) {
@@ -443,7 +706,9 @@ window.addEventListener("load", function () {
       startAudio.currentTime = 0;
       middleAudio.pause();
       middleAudio.currentTime = 0;
-      endAudio.play();
+      if (!debug) {
+        endAudio.play();
+      }
 
       return;
     }
@@ -455,78 +720,34 @@ window.addEventListener("load", function () {
       background.draw(ctx);
       background.update();
       player.draw(ctx);
-      player.update(input, deltaTime, enemies);
-      handleEnemies(deltaTime);
+      player.update(input, deltaTime, enemies, coins);
+      handleObjects(deltaTime);
       displayStatusText(ctx);
     }
     animationId = requestAnimationFrame(animate);
   }
 
-  const againFunction = (name) => {
-    endAudio.pause();
-    endAudio.currentTime = 0;
-    startAudio.play();
-    avatar = name;
-    gameSpeed = 14;
-    enemies = [];
-    gameOver = false;
-    score = 0;
-    input = new InputHandler();
-    player = new Player(canvas.width, canvas.height);
-    background = new Background(canvas.width, canvas.height);
-    lastTime = 0;
-    enemyTimer = 0;
-    enemyInterval = 1000;
-    randomEnemyInterval = Math.random() * 1000 + 500;
-    lastFrameTime = 0;
-    scoreCounter = 0;
-    gameOverMenu.classList.remove("show");
-    animate(0);
-  };
-
-  function setAvatar(name) {
-    endAudio.pause();
-    endAudio.currentTime = 0;
-    startAudio.play();
-    avatar = name;
-    input = new InputHandler();
-    player = new Player(canvas.width, canvas.height);
-    background = new Background(canvas.width, canvas.height);
-
-    lastTime = 0;
-    enemyTimer = 0;
-    enemyInterval = 100;
-    randomEnemyInterval = Math.random() * 1000 + 500;
-    animationId;
-    lastFrameTime = 0;
-    scoreCounter = 0;
-    mainMenu.classList.remove("show");
-    animate(0);
-  }
-
-  function randomNumberGen(min, max) {
-    // min and max included
-    return Math.random() * (max - min) + min;
-  }
-
-  document.getElementById("mc-btn").addEventListener("click", () => {
+  // Controls
+  mcButton.addEventListener("click", () => {
     setAvatar("mc");
   });
 
-  document.getElementById("skater-btn").addEventListener("click", () => {
+  skaterButton.addEventListener("click", () => {
     setAvatar("skater");
   });
 
-  document.getElementById("wizard-btn").addEventListener("click", () => {
+  wizardButton.addEventListener("click", () => {
     setAvatar("wizard");
   });
 
   againMc.addEventListener("click", () => {
     againFunction("mc");
   });
+
   againSkater.addEventListener("click", () => {
     againFunction("skater");
   });
+
   againWizard.addEventListener("click", () => {
     againFunction("wizard");
   });
@@ -535,8 +756,13 @@ window.addEventListener("load", function () {
   startAudio.addEventListener("ended", () => {
     middleAudio.play();
   });
+
   middleAudio.addEventListener("ended", function () {
     middleAudio.currentTime = 0;
     middleAudio.play();
   });
+
+  if (debugRun) {
+    initGame("mc");
+  }
 });
